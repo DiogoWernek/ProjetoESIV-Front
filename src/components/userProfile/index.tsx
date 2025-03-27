5d555823eb650f3f6f3bfb24a60cf60a9575cbfc
DiogoWernek <diogowernek86@gmail.com>
2025-03-26 21:56:33 -0300

    F

--- src/components/userProfile/index.tsx
+++ src/components/userProfile/index.tsx
@@ -1,32 +1,54 @@
-import * as S from './styles'
+import { useState, useEffect } from "react";
+import * as S from "./styles";
 
-interface sidebarStatus {
-  closed: string
+interface SidebarStatus {
+  closed: string;
 }
 
-const UserProfile = ({ closed }: sidebarStatus) => {
-  console.log(closed)
-  return(
-  <div style={{ position: 'relative'}}>
-    <S.Container closed={closed === 'closed'}>
-      <div className='user-photo'>
-        <p>D</p>  
-      </div>
-      <S.UserName closed={closed === 'closed'}>
-        <p>Deividi Costinha</p>
-      </S.UserName>
-    </S.Container>
+const UserProfile = ({ closed }: SidebarStatus) => {
+  const [show, setShow] = useState(false);
 
-    {closed === 'closed' ? (
-      <p 
-        style={{ padding: '0.3rem 0.5rem', borderRadius: "999px", position: 'absolute', backgroundColor: 'white' , top: '10px', 
-          right: '31%', color: '#2662D9' }}
-      >
-        D
-      </p>
-      ) : (<></>)}
-  </div>
-  )
-}
+  useEffect(() => {
+    if (closed === "closed") {
+      const timer = setTimeout(() => {
+        setShow(true);
+      }, 300);
+      return () => clearTimeout(timer);
+    } else {
+      setShow(false);
+    }
+  }, [closed]);
+
+  return (
+    <div style={{ position: "relative" }}>
+      <S.Container closed={closed === "closed"}>
+        <div className="user-photo">
+          <p>D</p>
+        </div>
+        <S.UserName closed={closed === "closed"}>
+          <p>Deividi Costinha</p>
+        </S.UserName>
+      </S.Container>
+
+      {closed === "closed" && (
+        <p
+          style={{
+            padding: "0.3rem 0.5rem",
+            borderRadius: "999px",
+            position: "absolute",
+            backgroundColor: "white",
+            top: "10px",
+            right: "31%",
+            color: "#2662D9",
+            opacity: show ? 1 : 0,
+            transition: "opacity 0.5s ease",
+          }}
+        >
+          D
+        </p>
+      )}
+    </div>
+  );
+};
 
 export default UserProfile;
